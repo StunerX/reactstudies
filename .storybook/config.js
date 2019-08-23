134f85d09f2f652c174dbc8e712bfdcf65ffb012
stunerx <cleyton.a.santos@gmail.com>
2019-08-22 23:03:11 -0300

    criando primera story no com storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
 import { configure } from '@kadira/storybook';
 
+const req = require.context('../src/components', true, /\.storybook.js/)
+
+
 function loadStories() {
-  require('../stories');
+  req.keys().forEach(file => {
+    req(file)
+  });
 }
 
 configure(loadStories, module);
---
+++ src/components/actions/action.storybook.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import { storiesOf } from '@kadira/storybook'
+import React from 'react'
+
+storiesOf('Div', module)
+    .add('First Story', () => (
+        <div>Hello, First Action Story</div>
+    ))
